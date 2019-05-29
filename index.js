cbe2b0321f3b674e960eb00a237a5a23f2ca768b
Vik Williamson <williamson.vik@gmail.com>
2019-05-29 03:09:02 -0400

    Add files via upload
    Upload latest index.html, index.js, and style.css

---
+++ index.js
@@ -0,0 +1,29 @@
+function updateTime() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        
+        if (minutes < 10){
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10){
+            seconds = "0" + seconds;
+        } 
+        if (hours > 13){
+          hours -= 12;
+        }
+        var displayTime = hours + ":" + minutes + " ";
+        if(hours > 11){
+            displayTime+="PM";
+        } else {
+            displayTime+="AM"
+        }
+        
+        setTimeout("updateTime()",1000);
+        document.getElementById("time").innerHTML=displayTime;
+    }
+    updateTime();
+
+var d = new Date();
+document.getElementById("date").innerHTML = d.toLocaleDateString();
